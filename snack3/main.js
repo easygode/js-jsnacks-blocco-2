54bb99d8aa26a1c2adf2c26393f5261fb052b401
easygode <bimalbenvenuti@gmail.com>
2023-02-27 12:35:15 +0100

    Snack3

---
+++ snack3/main.js
@@ -0,0 +1,10 @@
+const nomi = ['Paolo', 'Silvio', 'Anna', 'Costanza', 'Hotel', 'Telefon', 'Golf'];
+const cognomi  = ['Giullare', 'Kiloz', 'Giuitj', 'Fernemi', 'Giuop', 'Firnemr'];
+const invitati = [];
+
+for (let i = 0; i < 3; i++) {
+    randomNomi = Math.floor(Math.random() * nomi.length);
+    randomCognomi = Math.floor(Math.random() * cognomi.length);
+    
+    const invitati = `$randomNomi`
+}
