5e43bf0c581ccbd24bfa9f167ce393f83af9ef45
easygode <bimalbenvenuti@gmail.com>
2023-02-27 12:04:27 +0100

    correzione

--- snack2/main.js
+++ snack2/main.js
@@ -1,11 +1,8 @@
-let num = 0
-
-for (let i = 0; i < 3; i++) {
-    let num = (Number ('Inserisci un numero'));
-}
+const num = prompt (Number('Inserisci un numero'));
 
 if (num % 2 === 0) {
  console.log(num);
 } else {
-    console.log (prompt + 1);
+    numPlusone = num + 1;
+    console.log (numPlusone);
 }
