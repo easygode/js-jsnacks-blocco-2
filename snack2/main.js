b298758d2ebb07d67501884f8bdcd402b9bf3bc3
easygode <bimalbenvenuti@gmail.com>
2023-02-27 11:59:10 +0100

    first push

---
+++ snack2/main.js
@@ -0,0 +1,11 @@
+let num = 0
+
+for (let i = 0; i < 3; i++) {
+    let num = (Number ('Inserisci un numero'));
+}
+
+if (num % 2 === 0) {
+ console.log(num);
+} else {
+    console.log (prompt + 1);
+}
