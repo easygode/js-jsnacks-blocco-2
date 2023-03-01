14848fc68d095af3d46b6f30ac1450f60a2db0c7
easygode <bimalbenvenuti@gmail.com>
2023-03-01 11:28:35 +0100

    con condizione else is NaN

--- snack1/main.js
+++ snack1/main.js
@@ -1,4 +1,9 @@
 
+//Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+//Esegui questo programma in due versioni, con il for e con il while.
+
+//ciclo while
 let sum = 0;
 let count = 1;
 
@@ -9,7 +14,7 @@ while (count < 6) {
     if(!isNaN(input)) {
         sum += input;
         count++;
-    }
+    } else {Number(prompt(`Inserisci un numero (${count})`));}
 }
 
 alert(sum);
